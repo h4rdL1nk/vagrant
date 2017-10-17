d4a9b3df9345ebd4b01e82032312ef7db76f03fd
h4rdL1nk <imthedanger@protonmail.com>
2017-10-17 16:49:23 +0200

    Added VPC info

--- examples/data_structs.rb
+++ examples/data_structs.rb
@@ -59,20 +59,28 @@ SERVERS = [
 ]
 
 AWS = {
-        :subnets => [
-                {
-                        :id => 'subnet-5c6cad37',
-                        :cidr => '10.172.88.0/24'
-                },
-                {
-                        :id => 'subnet-366a6b4c',
-                        :cidr => '10.172.89.0/24'
-                },
-                {
-                        :id => 'subnet-ee21daa3',
-                        :cidr => '10.172.90.0/24'
-                }
-        ]
+        :credentials => {
+            :region => 'eu-west-1',
+            :keyid => '',
+            :accesskey => ''   
+        },
+        :vpc => {
+            :id => 'vpc-8decd1e5',
+            :subnets => [
+                    {
+                            :id => 'subnet-5c6cad37',
+                            :cidr => '10.172.88.0/24'
+                    },
+                    {
+                            :id => 'subnet-366a6b4c',
+                            :cidr => '10.172.89.0/24'
+                    },
+                    {
+                            :id => 'subnet-ee21daa3',
+                            :cidr => '10.172.90.0/24'
+                    }
+            ]
+        }
 }
 
 
