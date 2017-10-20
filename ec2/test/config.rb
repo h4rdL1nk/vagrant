b943fcc5a117c5a42789ca54ddafd2683c78e052
h4rdL1nk <imthedanger@protonmail.com>
2017-10-20 09:10:14 +0200

    Added swarm variables

--- ec2/test/config.rb
+++ ec2/test/config.rb
@@ -1,53 +1,54 @@
 
 SERVERS = [
+
     {
-	:role => 'master',
-	:hostname => 'swarm-test-1',
-	:aws => {
-		:ec2 => {
-			:type => 't2.nano',
-			:keypair => 'KP-inftel-pro',
-			:securitygroups => [
-				'sg-e966e691'
-			],
-			:ami => {
-				:id => 'ami-eed00d97',
-				:os => 'ubuntu',
-				:ssh => {
-					:user => 'ubuntu'
-				}	
-			},
-			:network => {
-				:subnet => 'subnet-4a0c0f2e',
-				:public => true,
-				:private => '10.172.57.240'
-			}
-		}
-	}
+        :role => 'master',
+        :hostname => 'swarm-test-1',
+        :aws => {
+            :ec2 => {
+                :type => 't2.nano',
+                :keypair => 'KP-inftel-pro',
+                :securitygroups => [
+                    'sg-e966e691'
+                ],
+                :ami => {
+                    :id => 'ami-eed00d97',
+                    :os => 'ubuntu',
+                    :ssh => {
+                        :user => 'ubuntu'
+                    }	
+                },
+                :network => {
+                    :subnet => 'subnet-4a0c0f2e',
+                    :public => true,
+                    :private => '10.172.57.240'
+                }
+            }
+        }
     },
     {
         :role => 'worker',
         :hostname => 'swarm-test-2',
         :aws => {
-                :ec2 => {
-                        :type => 't2.nano',
-                        :keypair => 'KP-inftel-pro',
-                        :securitygroups => [
-                                'sg-e966e691'
-                        ],
-                        :ami => {
-                                :id => 'ami-eed00d97',
-                                :os => 'ubuntu',
-                                :ssh => {
-                                        :user => 'ubuntu'
-                                }
-                        },
-                        :network => {
-                                :subnet => 'subnet-6c50511a',
-                                :public => true,
-                                :private => '10.172.58.240'
-                        }
+            :ec2 => {
+                :type => 't2.nano',
+                :keypair => 'KP-inftel-pro',
+                :securitygroups => [
+                    'sg-e966e691'
+                ],
+                :ami => {
+                    :id => 'ami-eed00d97',
+                    :os => 'ubuntu',
+                    :ssh => {
+                        :user => 'ubuntu'
+                    }
+                },
+                :network => {
+                    :subnet => 'subnet-6c50511a',
+                    :public => true,
+                    :private => '10.172.58.240'
                 }
+            }
         }
     }
 ]
